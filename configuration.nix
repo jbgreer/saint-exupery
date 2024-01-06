ad36da9b58db3b9d77992d0c8b23254b62986761
James B. Greer <jbgreer@gmail.com>
2024-01-06 10:10:48 -0600

    initial files

---
+++ configuration.nix
@@ -0,0 +1,53 @@
+# configuration_bootstrap.nix
+{ config, pkgs, lib, ... }: {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader settings
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.supportedFilesystems = [ "btrfs" "ntfs" "fat32" ];
+  hardware.enableAllFirmware = true;
+
+  # TODO set hostname
+  networking.hostName = "saint-exupery";
+  networking.networkmanager.enable = true;
+
+  # TODO set locale and timezone
+  i18n.defaultLocale = "en_US.utf8";
+  time.timeZone = "America/Chicago";
+
+  # TODO set username
+  users.users.jbgreer = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      pfetch
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Install packages - vim & git are very useful
+  environment.systemPackages = with pkgs; [
+    curl
+    git
+    neovim
+  ];
+
+  # set neovim as default editor
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+  
+  # WARNING! Be careful when changing.
+  system.stateVersion = "23.11";
+
+  # Enable use of flakes
+  nix.settings.experimental-features = "nix-command flakes";
+}
---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  desccription = "jbgreer flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs.nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations = {
+        saint-exupery = lib.nixosSystem {
+	  inherit system;
+	  modules = [ ./configuration.nix ];
+        };
+      };
+    };
+}
