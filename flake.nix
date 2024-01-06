0e575d158b46957ddf359873b46a32d5344cb19b
James B. Greer <jbgreer@gmail.com>
2024-01-06 10:46:37 -0600

    added framework 13 7040 amd hardware module

--- flake.nix
+++ flake.nix
@@ -3,18 +3,25 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+
+    nixos-hardware.url = "github:NixOS//nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, nixos-hardware, ... }:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
     in {
       nixosConfigurations = {
+
         saint-exupery = lib.nixosSystem {
 	  inherit system;
-	  modules = [ ./configuration.nix ];
+	  modules = [ 
+	    nixos-hardware.nixosModules.framework-13-7040-amd
+	    ./configuration.nix 
+	  ];
         };
+
       };
     };
 }
