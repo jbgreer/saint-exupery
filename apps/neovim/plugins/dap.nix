13104109ec091050b33d3d0f3113afceab8b08aa
James B. Greer <jbgreer@gmail.com>
2024-02-11 11:40:22 -0600

    trying to fix nvim elixir-ls dap

--- apps/neovim/plugins/dap.nix
+++ apps/neovim/plugins/dap.nix
@@ -4,46 +4,53 @@
 
     adapters = {
       executables = {
-        elixir = {
-          args = "--trace";
+        mix_task = {
+          #args = [ "--trace" ];
           command = "elixir-debugger";
-          options = {
-            cwd =  null;
-            detached =  true;
-            env = null;
-          };
+          #options = {
+          #  cwd =  null;
+          #  detached =  true;
+          #  env = null;
+          #};
         };
       };
 
-      servers = {
-        elixir = {
-          executable = {
-            args = null;
-            command = "";
-            cwd = "";
-            detached = true;
-          };
-          options.maxRetries = 14;
-        };
-      };
+      #servers = {
+        #elixir = {
+          #executable = {
+            #args = null;
+            #command = "";
+            #cwd = "";
+            #detached = true;
+          #};
+          #options.maxRetries = 14;
+        #};
+      #};
     };
 
     configurations = {
       elixir = [
         {
+        type = "mix_task";
+        name = "mix (Default tasks)";
+        request = "launch";
+        startApps = true;
+        }
+        {
           type = "mix_task";
           name = "mix test";
           request = "launch";
-          taskArgs = { "--trace" };
+          #taskArgs = [ "--trace" ];
           startApps = true;
-          projectDir = "${workspaceRoot}";
-          requireFiles = {
-            "test/**/testhelper.exs",
+          #projectDir = "${workspaceRoot}";
+          requireFiles = [
+            "test/**/test_helper.exs"
             "test/**/*_test.exs"
-          },
+          ];
         }
       ];
     };
-
   };
+
+  programs.nixvim.plugins.dap.extensions.dap-ui.enable = true;
 }
