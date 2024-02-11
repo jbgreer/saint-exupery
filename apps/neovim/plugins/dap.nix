cbd1ff19263039fd8dfac0b2e3eca34fa5cec35c
James B. Greer <jbgreer@gmail.com>
2024-02-11 11:39:46 -0600

    updated, breakage in hyprland commented out

--- apps/neovim/plugins/dap.nix
+++ apps/neovim/plugins/dap.nix
@@ -1,5 +1,49 @@
 {
   programs.nixvim.plugins.dap = {
     enable = true;
+
+    adapters = {
+      executables = {
+        elixir = {
+          args = "--trace";
+          command = "elixir-debugger";
+          options = {
+            cwd =  null;
+            detached =  true;
+            env = null;
+          };
+        };
+      };
+
+      servers = {
+        elixir = {
+          executable = {
+            args = null;
+            command = "";
+            cwd = "";
+            detached = true;
+          };
+          options.maxRetries = 14;
+        };
+      };
+    };
+
+    configurations = {
+      elixir = [
+        {
+          type = "mix_task";
+          name = "mix test";
+          request = "launch";
+          taskArgs = { "--trace" };
+          startApps = true;
+          projectDir = "${workspaceRoot}";
+          requireFiles = {
+            "test/**/testhelper.exs",
+            "test/**/*_test.exs"
+          },
+        }
+      ];
+    };
+
   };
 }
